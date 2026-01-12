a3325900ce26830acac807b11b9624ec911f4005
Dhruv Patel <dhruvp7225@gmail.com>
2026-01-12 09:27:57 +0530

    Changed links

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-// menu.addEventListener('click',()=>{
-
-//     const navbar = document.querySelector('.navbar')
-//     let x = navbar.style.display
-//     const menu=document.getElementById("menu")
-//     if(x !== 'block') {
-//         navbar.style.display="block"
-//         document.querySelector("#menu-img").setAttribute("src","./assets/images/remove.png")
-//         document.querySelector("#menu-img").setAttribute("width","5%")
-        
-//         // menu.style.display="none"
-//     }  else{
-//         navbar.style.display="none"
-//         document.querySelector("#menu-img").setAttribute("src","./assets/images/menu-01.png")
-//         document.querySelector("#menu-img").setAttribute("width","3%")
-//         menu.style.background="transparent"
-//         menu.style.border="none"
-//         // menu.style.zIndex="18"
-//         // document.querySelector("#menu-img").setAttribute("width","10%")
-//         // menu.style.display="block"
-//     } 
-// })
-
 function closesidebar(){
     const sidebar=document.querySelector(".sidebar")
     sidebar.style.display="none";
